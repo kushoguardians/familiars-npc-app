63481682fff9753c3331f670f1cec275eccbf2bd
exidz <exidz@protonmail.com>
2024-11-17 00:38:36 +0800

    chore: types and contants

---
+++ src/shared/types.ts
@@ -0,0 +1,13 @@
+export type NPCEquipItems = {
+  head: bigint
+  mouth: bigint
+}
+
+export type NPCStats = {
+  0: number
+  1: string
+  2: bigint
+  3: bigint
+  4: bigint
+  5: NPCEquipItems
+}
