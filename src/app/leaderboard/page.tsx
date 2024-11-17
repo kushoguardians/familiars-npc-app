3dd1520f5f06cff57bcb4a176ade96b420c7911d
exidz <exidz@protonmail.com>
2024-11-17 12:50:57 +0800

    style: leaderboard ui

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,11 +1,13 @@
+import Leaderboard from "./components/leaderboard";
+
 export default async function Page() {
   let data = await fetch('http://localhost:3000/api/multicall/npc-stats', {
     method: 'post',
     body: JSON.stringify({tokenIds: [1, 2, 3, 4]}),
   })
   const npcStats = await data.json()
-  console.log(npcStats);
+
   
 
-  return <></>
+  return (<Leaderboard data={npcStats.data} />)
 }
